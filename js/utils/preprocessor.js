c98a0bc7dbf2918da848b0aa9e525221f77c110a
Pedro Santos <donbonifacio@gmail.com>
2015-01-29 16:52:30 +0000

    Optimize jest

--- js/utils/preprocessor.js
+++ js/utils/preprocessor.js
@@ -1,6 +1,7 @@
 var ReactTools = require('react-tools');
 module.exports = {
-  process: function(src) {
+  process: function(src, file) {
+    if(!file.match(/\.react\.js$/)) return src;
     return ReactTools.transform(src);
   }
 };
