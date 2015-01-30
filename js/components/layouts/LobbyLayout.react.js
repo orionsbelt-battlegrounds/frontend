596c0be3d85e75b31a3562f996ce6166e97ec4f0
Pedro Santos <donbonifacio@gmail.com>
2015-01-30 12:26:46 +0000

    Minor adjustments

--- js/components/layouts/LobbyLayout.react.js
+++ js/components/layouts/LobbyLayout.react.js
@@ -10,7 +10,7 @@ var LobbyLayout = React.createClass({
       <div className="row">
       <ul className="nav nav-tabs">
         <li className="active"><a href="#openGames" data-toggle="tab">Open Games</a></li>
-        <li><a href="#yourGames" data-toggle="tab">Your Games</a></li>
+        <li><a href="#yourGames" data-toggle="tab">Your Games <span className="label label-warning">3 games to play</span></a></li>
       </ul>
       <div id="myTabContent" className="tab-content">
         <div className="tab-pane fade active in" id="openGames">
