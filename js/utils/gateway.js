f770f8868376baa996b838aa0dc1d52182149db3
Pedro Santos <donbonifacio@gmail.com>
2015-02-11 21:59:38 +0000

    Refactor gateway

--- js/utils/gateway.js
+++ js/utils/gateway.js
@@ -2,58 +2,44 @@ var _ = require("mori");
 
 var OBB_API_URL = "http://api.orionsbelt.eu";
 
-function makeAnonymousRequest(path, callback) {
+function makeRequest(user, method, path, data, callback) {
   var url = OBB_API_URL + path;
-  if(window['$']) {
-    $.getJSON(url, callback);
+  var headers = null;
+  if(user) {
+    headers = {
+      "x-obb-auth-token" : _.get(user, "token")
+    };
   }
+  var postData = null;
+  if(data) {
+    postData = JSON.stringify(data);
+  }
+  if(window['$']) {
+    $.ajax({
+      type: method,
+      url: url,
+      contentType: "application/json",
+      data: postData,
+      headers: headers,
+      success: callback
+    });
+  }
+}
+
+function makeAnonymousRequest(path, callback) {
+  makeRequest(null, "GET", path, null, callback);
 }
 
 function getRequest(user, path, callback) {
-  var url = OBB_API_URL + path;
-  if(window['$']) {
-    $.ajax({
-      type: "GET",
-      url: url,
-      contentType: "application/json",
-      headers: {
-        "x-obb-auth-token" : _.get(user, "token")
-      },
-      success: callback
-    });
-  }
+  makeRequest(user, "GET", path, null, callback);
 }
 
 function postRequest(user, path, data, callback) {
-  var url = OBB_API_URL + path;
-  if(window['$']) {
-    $.ajax({
-      type: "POST",
-      url: url,
-      data: JSON.stringify(data),
-      contentType: "application/json",
-      headers: {
-        "x-obb-auth-token" : _.get(user, "token")
-      },
-      success: callback
-    });
-  }
+  makeRequest(user, "POST", path, data, callback);
 }
 
 function putRequest(user, path, data, callback) {
-  var url = OBB_API_URL + path;
-  if(window['$']) {
-    $.ajax({
-      type: "PUT",
-      url: url,
-      data: JSON.stringify(data),
-      contentType: "application/json",
-      headers: {
-        "x-obb-auth-token" : _.get(user, "token")
-      },
-      success: callback
-    });
-  }
+  makeRequest(user, "PUT", path, data, callback);
 }
 
 module.exports = {
