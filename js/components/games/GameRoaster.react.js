b6b0f38b0d1ba5630d0b7777b1b0d43d4b284da7
Pedro Santos <donbonifacio@gmail.com>
2015-03-04 14:38:52 +0000

    Highlight player to play

--- js/components/games/GameRoaster.react.js
+++ js/components/games/GameRoaster.react.js
@@ -12,18 +12,28 @@ module.exports = React.createClass({
 
   render: function () {
     var game = this.props.game;
+    var state = _.getIn(game, ["board", "state"])
+
     var p1 = _.getIn(game, ["p1", "name"]);
     var p2 = _.getIn(game, ["p2", "name"]);
+
+    var css1 = state === "deploy" || state === "p1" ? "active" :"";
+    var css2 = state === "deploy" || state === "p2" ? "active" :"";
 
     if(this.props.playerCode && this.props.playerCode === "p1") {
       p1 = _.getIn(game, ["p2", "name"]);
       p2 = _.getIn(game, ["p1", "name"]);
+      css2 = state === "deploy" || state === "p1" ? "active" :"";
+      css1 = state === "deploy" || state === "p2" ? "active" :"";
     }
+
+    css1 = "list-group-item " + css1;
+    css2 = "list-group-item " + css2;
 
     return (
       <div className="list-group">
-        <ProfileLink username={p1} className="list-group-item"/>
-        <ProfileLink username={p2} className="list-group-item"/>
+        <ProfileLink username={p1} className={css1}/>
+        <ProfileLink username={p2} className={css2}/>
       </div>
     );
   }
